8f4c7d84a2302c2fea8e05f245e6db04c85d9479
chaya-bachrach <72816652+chaya-bachrach@users.noreply.github.com>
2021-02-18 21:27:39 +0200

    Update App.js

--- react-english-web/src/App.js
+++ react-english-web/src/App.js
@@ -4,9 +4,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import SignUp from './components/pages/SignUp';
-import Cards2 from './components/Cards2'
-import Test from './components/pages/Test'
 import Cards2 from './components/Cards2';
+import Test from './components/pages/Test';
 import Learn from './components/pages/Learn';
 
 function App() {
