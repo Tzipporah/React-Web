fff8f41f23d18d8f4b09d846543db66e3ce3151d
Tzipporah <52968863+Tzipporah@users.noreply.github.com>
2021-02-23 22:07:24 +0200

    add some css for learn.js
    added category section for all category to benefit from

---
+++ react-english-web/src/components/CategorySection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css';
+import './CategorySection.css';
+
+function CategorySection(props) {
+
+  
+  return (
+    <div className='category-container'>
+      
+      <video src={props.videoLink} autoPlay loop muted />
+      <h3>{props.title}</h3>
+      <div className='category-btns'>
+          {props.children}
+      </div>
+    </div>
+  );
+}
+
+export default CategorySection;
